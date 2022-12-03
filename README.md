799fc464a7de2d9f6fd30531bb6e79a3ae528520
lavender0104 <worklavender0104@gmail.com>
2022-12-03 17:48:39 +0800

    Restoring deleted repository

---
+++ README.md
@@ -0,0 +1,7 @@
+# MERN LAVENDERSHOP
+
+# PRODUCED BY LOO KAH YEW AND LEE VOON KANG
+
+# TARUMT FYP 2022
+
+Testing on pushing local master to remote branch
---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">lavendershop</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
