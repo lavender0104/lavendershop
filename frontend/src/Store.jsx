5b0bdbcc187f2347feb2f242d9a90d1e3567a328
lavender0104 <worklavender0104@gmail.com>
2022-12-09 20:51:09 +0800

    Added Shipping and Checkout

--- frontend/src/Store.jsx
+++ frontend/src/Store.jsx
@@ -8,6 +8,9 @@ const initialState = {
     : null,
 
   cart: {
+    shippingAddress: localStorage.getItem("shippingAddress")
+      ? JSON.parse(localStorage.getItem("shippingAddress"))
+      : {},
     cartItems: localStorage.getItem("cartItems")
       ? JSON.parse(localStorage.getItem("cartItems"))
       : [],
@@ -46,7 +49,22 @@ const reducer = (state, action) => {
     case "USER_SIGNIN":
       return { ...state, userInfo: action.payload };
     case "USER_SIGNOUT":
-      return { ...state, userInfo: null };
+      return {
+        ...state,
+        userInfo: null,
+        cart: {
+          cartItems: [],
+          shippingAddress: {},
+        },
+      };
+    case "SAVE_SHIPPING_ADDRESS":
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          shippingAddress: action.payload,
+        },
+      };
     default:
       return state;
   }
